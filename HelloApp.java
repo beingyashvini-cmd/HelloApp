c8f3c0dc15f9c33e319fc7a2b51589985dba9cb0
ys6573-dot <Ys6573@srmist.edu.in>
2026-03-17 09:08:28 +0530

    completed UC1:Hello World

---
+++ HelloApp.java
@@ -0,0 +1,6 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        // Output text to the console as per UC1 requirements
+        System.out.println("Hello World");
+    }
+}
